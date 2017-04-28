3230e00ae0397e893afa6df7114ad9500ff90a96
molpopgen <krthornt@uci.edu>
2017-04-28 14:14:04 -0700

    minor plotting tweaks

--- constant_selection/plot_means.R
+++ constant_selection/plot_means.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(viridis)
 x = read.table("data.txt",header=T)
 
 plot_labeller <- function(variable,value){
@@ -8,9 +9,10 @@ plot_labeller <- function(variable,value){
       return(paste("h = ",value,sep=""))
   } 
 }
-p = ggplot(x,aes(x=factor(pneut),times,color=Simulation)) +  #+ geom_boxplot(aes(color=sim,position="dodge")) + 
+p = ggplot(x,aes(x=factor(pneut),times)) +  #+ geom_boxplot(aes(color=sim,position="dodge")) + 
     facet_grid(h~s,scales="free_y",labeller=plot_labeller) + 
-    geom_point(position = position_jitter(width = 0.2)) + theme_bw()  +
-    xlab("Proportion of new mutations that are neutral.") + ylab("Run time (hours)")
+    geom_point(position = position_jitter(width = 0.2),aes(color=Simulation),size=2) + 
+    xlab("Proportion of new mutations that are neutral.") + ylab("Run time (hours)") +
+    scale_fill_viridis(discrete=TRUE) + theme_bw()
 
 ggsave("test.png",p)
